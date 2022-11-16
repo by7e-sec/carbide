560d82693634ee9504fb4ae34cf774c479a1cc43
by7e-sec <by7e.sec@protonmail.com>
2022-11-16 23:26:05 +0100

    Tiny cleanup

--- src/libs/processor.py
+++ src/libs/processor.py
@@ -20,14 +20,16 @@ class Processor:
         # Colors
         bright_green = Style.BRIGHT + Fore.GREEN
         dim_red = Style.DIM + Fore.RED
-        rst = Style.RESET_ALL
 
         bps = self.blueprints.list_all()
         print(Style.BRIGHT + Fore.MAGENTA + "Available blueprints:")
         for bp in bps:
             is_valid = bright_green + 'Valid' if bps[bp]['valid'] else dim_red + 'Not valid'
             is_active = bright_green + 'Active' if bps[bp]['active'] else dim_red + 'Inactive'
-            print(Style.BRIGHT + Fore.YELLOW + bp + ': ' + is_valid + Fore.WHITE + ' || ' + is_active + rst)
+            is_valid = is_valid + Style.RESET_ALL
+            is_active = is_active + Style.RESET_ALL
+
+            print(Style.BRIGHT + Fore.YELLOW + bp + ': ' + is_valid + Fore.WHITE + ' || ' + is_active)
             if bps[bp]['description'] != '':
                 print(Style.DIM + Fore.WHITE + ' # ' + Fore.MAGENTA + bps[bp]['description'])
             else:
